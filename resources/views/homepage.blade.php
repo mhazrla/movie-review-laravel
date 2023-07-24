3dddfe68dde6d7363c85f01182f8feb919cd8157
mhazrla <mhazrla@gmail.com>
2023-07-24 23:47:36 +0700

    all

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -2,39 +2,41 @@
     <x-slot name="header">
         <div class="relative isolate px-6 pt-6 lg:px-8">
             @include('layouts.navigation')
-
-            <div class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+            <div class=" absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
                 aria-hidden="true">
                 <div class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
                     style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)">
                 </div>
             </div>
 
-            <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-24">
-                <div class="hidden sm:mb-8 sm:flex sm:justify-center">
-                    <div
-                        class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                        Announcing our movie review website. <a href="#" class="font-semibold text-indigo-600"><span
-                                class="absolute inset-0" aria-hidden="true"></a>
+            <div class="my-8 hero min-h-screen px-0">
+                <div class="hero-overlay bg-opacity-60"></div>
+                <div class="hero-content text-center text-neutral-content">
+                    <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-24">
+                        <div class="hidden sm:mb-8 sm:flex sm:justify-center ">
+                            <div
+                                class="relative rounded-full px-3 py-1 text-sm leading-6 text-white ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+                                Announcing our movie review website. <a href="#"
+                                    class="font-semibold text-indigo-600"><span class="absolute inset-0"
+                                        aria-hidden="true"></a>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Social film
+                                discovery</h1>
+                            <p class="mt-6 text-lg leading-8 text-gray-300">ScreenpunditID is a movie recommendation and
+                                review
+                                platform that offers admin-generated reviews. It also provides personalized movie
+                                recommendations based on your preferences.
+                            </p>
+                        </div>
                     </div>
+
                 </div>
-                <div class="text-center">
-                    <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Social film discovery</h1>
-                    <p class="mt-6 text-lg leading-8 text-gray-600">ScreenpunditID is a movie recommendation and review
-                        platform that offers admin-generated reviews. It also provides personalized movie
-                        recommendations based on your preferences.
-                    </p>
-                    {{-- <div class="mt-10 flex items-center justify-center gap-x-6">
-                        <a href="#"
-                            class="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Get
-                            started</a>
-                        <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Learn more <span
-                                aria-hidden="true">→</span></a>
-                    </div> --}}
-                </div>
+
             </div>
 
-            <div class="py-12">
+            <div class="py-12 ">
                 <h2 class="font-semibold text-xl text-gray-800 leading-tight">
                     {{ __('Movies Review') }}
                 </h2>
